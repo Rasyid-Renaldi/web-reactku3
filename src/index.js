cc7d38cb02ab59ee43d6a938b3e0b4d86593d447
Rasyid-deve <renaldirasyed@gmail.com>
2022-03-08 12:41:08 +0700

    Update Praktikum 3 newest

--- src/index.js
+++ src/index.js
@@ -2,14 +2,88 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HelloComponent from './component/HelloComponent';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// function HelloComponent() {
+  //   return (
+  //     <div>
+  //     <p>HelloComponent</p>
+  //     </div>
+  //   );
+  // }
+  ReactDOM.render(<HelloComponent/>, document.getElementById('root'));
+  
+  
+  // Membuat class
+  // class StateFullComponent extends React.Component {
+  //   render() {
+  //     return (
+  //       <div>
+  //         <h1>StateFullComponent</h1>
+  //       </div>
+  //     );
+  //   }
+  // }
+  // ReactDOM.render(<StateFullComponent/>, document.getElementById('root'));
+
+// class Test extends React.Component {  
+//   constructor(props)  
+//   {  
+//       super(props);  
+//       this.state = { hello : "World!" };  
+//   }  
+ 
+//   componentWillMount()  
+//   {  
+//       console.log("componentWillMount()");  
+//   }  
+ 
+//   componentDidMount()  
+//   {  
+//       console.log("componentDidMount()");  
+//   }  
+ 
+//   changeState()  
+//   {  
+//       this.setState({ hello : "Geek!" });  
+//   }  
+ 
+//   render()  
+//   {  
+//       return (  
+//           <div>  
+//           <h1>GeeksForGeeks.org, Hello{ this.state.hello }</h1>  
+//           <h2>  
+//           <a onClick={this.changeState.bind(this)}>Press Here!</a>  
+//           </h2>  
+//           </div>);  
+//   }  
+ 
+//   shouldComponentUpdate(nextProps, nextState)  
+//   {  
+//       console.log("shouldComponentUpdate()");  
+//       return true;  
+//   }  
+ 
+//   componentWillUpdate()  
+//   {  
+//       console.log("componentWillUpdate()");  
+//   }  
+ 
+//   componentDidUpdate()  
+//   {  
+//       console.log("componentDidUpdate()");  
+//   }  
+// }   
+// ReactDOM.render(<Test />, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
