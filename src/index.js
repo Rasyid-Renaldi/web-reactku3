25eb7c2eaea40cd686f9869b63dc8fd505d1a773
Rasyid-deve <renaldirasyed@gmail.com>
2022-03-16 12:23:39 +0700

    Update BlogPost

--- src/index.js
+++ src/index.js
@@ -1,81 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-import HelloComponent from './component/HelloComponent';
+// import HelloComponent from './component/HelloComponent';
 import reportWebVitals from './reportWebVitals';
 
-// function HelloComponent() {
-  //   return (
-  //     <div>
-  //     <p>HelloComponent</p>
-  //     </div>
-  //   );
-  // }
-  ReactDOM.render(<HelloComponent/>, document.getElementById('root'));
-  
-  
-  // Membuat class
-  // class StateFullComponent extends React.Component {
-  //   render() {
-  //     return (
-  //       <div>
-  //         <h1>StateFullComponent</h1>
-  //       </div>
-  //     );
-  //   }
-  // }
-  // ReactDOM.render(<StateFullComponent/>, document.getElementById('root'));
+import BlogPost from './container/BlogPost/BlogPost';
 
-// class Test extends React.Component {  
-//   constructor(props)  
-//   {  
-//       super(props);  
-//       this.state = { hello : "World!" };  
-//   }  
- 
-//   componentWillMount()  
-//   {  
-//       console.log("componentWillMount()");  
-//   }  
- 
-//   componentDidMount()  
-//   {  
-//       console.log("componentDidMount()");  
-//   }  
- 
-//   changeState()  
-//   {  
-//       this.setState({ hello : "Geek!" });  
-//   }  
- 
-//   render()  
-//   {  
-//       return (  
-//           <div>  
-//           <h1>GeeksForGeeks.org, Hello{ this.state.hello }</h1>  
-//           <h2>  
-//           <a onClick={this.changeState.bind(this)}>Press Here!</a>  
-//           </h2>  
-//           </div>);  
-//   }  
- 
-//   shouldComponentUpdate(nextProps, nextState)  
-//   {  
-//       console.log("shouldComponentUpdate()");  
-//       return true;  
-//   }  
- 
-//   componentWillUpdate()  
-//   {  
-//       console.log("componentWillUpdate()");  
-//   }  
- 
-//   componentDidUpdate()  
-//   {  
-//       console.log("componentDidUpdate()");  
-//   }  
-// }   
+// function HelloComponent() {
+//   return (
+//     <div>
+//     <p>HelloComponent</p>
+//     </div>
+//   );
+// }
+// ReactDOM.render(<HelloComponent/>, document.getElementById('root'));
+ReactDOM.render(<BlogPost />, document.getElementById('content')); // parameter "content" untuk menampilkan image pada file BlogPost.jsx
+// ReactDOM.render(<BlogPost />, document.getElementById('root'));
+
+// Membuat class
+// class StateFullComponent extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>StateFullComponent</h1>
+//       </div>
+//     );
+//   }
+// }
+// ReactDOM.render(<StateFullComponent/>, document.getElementById('root'));
+
+// class Test extends React.Component {
+//   constructor(props)
+//   {
+//       super(props);
+//       this.state = { hello : "World!" };
+//   }
+
+//   componentWillMount()
+//   {
+//       console.log("componentWillMount()");
+//   }
+
+//   componentDidMount()
+//   {
+//       console.log("componentDidMount()");
+//   }
+
+//   changeState()
+//   {
+//       this.setState({ hello : "Geek!" });
+//   }
+
+//   render()
+//   {
+//       return (
+//           <div>
+//           <h1>GeeksForGeeks.org, Hello{ this.state.hello }</h1>
+//           <h2>
+//           <a onClick={this.changeState.bind(this)}>Press Here!</a>
+//           </h2>
+//           </div>);
+//   }
+
+//   shouldComponentUpdate(nextProps, nextState)
+//   {
+//       console.log("shouldComponentUpdate()");
+//       return true;
+//   }
+
+//   componentWillUpdate()
+//   {
+//       console.log("componentWillUpdate()");
+//   }
+
+//   componentDidUpdate()
+//   {
+//       console.log("componentDidUpdate()");
+//   }
+// }
 // ReactDOM.render(<Test />, document.getElementById('root'));
 
 // ReactDOM.render(
