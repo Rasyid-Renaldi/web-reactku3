cf355aa07edd77d41f1bac46816e53162359a893
Rasyid-deve <renaldirasyed@gmail.com>
2022-03-16 16:12:34 +0700

    Update Praktikum 3

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -28,29 +28,35 @@ import Post from '../../component/BlogPost/Post';
 
 class BlogPost extends Component {
   state = {
-    // Komponen state dari React untuk statefull component
-    listArtikel: [], // Varibel array yang digunakan untuk menyimpan data API
+    listArtikel: [],
+  };
+
+  ambilDataDariServerAPI = () => {
+    fetch('http://localhost:3001/posts')
+      .then((response) => response.json())
+      .then((jsonHasilAmbilDariAPI) => {
+        this.setState({
+          listArtikel: jsonHasilAmbilDariAPI
+        })
+      })
   };
 
   componentDidMount() {
-    // Komponen untuk mengecek ketika component telah di-mount-ing, maka panggil API
-    fetch('http://localhost:3001/posts') // Alamat URL API yang ingin kita ambil datanya
-      .then((response) => response.json()) // Ubah response data dari URL API menjadi sebuah data json
-      .then((json) => {
-        // Data json hasil ambil dari API ketika masukkan ke dalam listArtikel pada state
-        this.setState({
-          listArtikel: json,
-        });
-      });
+    this.ambilDataDariServerAPI();
   }
+
+  handleHapusArtikel = (data) => {
+    fetch(`http://localhost:3001/posts/${data}`, { method: 'DELETE' }).then((res) => {
+      this.ambilDataDariServerAPI();
+    });
+  };
 
   render() {
     return (
       <div className="post-artikel">
         <h2>Daftar Artikel</h2>
         {this.state.listArtikel.map((artikel) => {
-          // looping dan masukkan untuk setiap data yang ada di listArtikel ke variabel artikel
-          return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} />; // Mappingkan data json dari API sesuai dengan kategorinya
+          return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel} />;
         })}
       </div>
     );
@@ -59,4 +65,38 @@ class BlogPost extends Component {
 
 export default BlogPost;
 
-// https://jsonplaceholder.typicode.com/posts
+
+// class BlogPost extends Component {
+//   state = {
+//     // Komponen state dari React untuk statefull component
+//     listArtikel: [], // Varibel array yang digunakan untuk menyimpan data API
+//   };
+
+//   componentDidMount() {
+//     // Komponen untuk mengecek ketika component telah di-mount-ing, maka panggil API
+//     fetch('http://localhost:3001/posts') // Alamat URL API yang ingin kita ambil datanya
+//       .then((response) => response.json()) // Ubah response data dari URL API menjadi sebuah data json
+//       .then((json) => {
+//         // Data json hasil ambil dari API ketika masukkan ke dalam listArtikel pada state
+//         this.setState({
+//           listArtikel: json,
+//         });
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <div className="post-artikel">
+//         <h2>Daftar Artikel</h2>
+//         {this.state.listArtikel.map((artikel) => {
+//           // looping dan masukkan untuk setiap data yang ada di listArtikel ke variabel artikel
+//           return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} />; // Mappingkan data json dari API sesuai dengan kategorinya
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+// export default BlogPost;
+
+// // https://jsonplaceholder.typicode.com/posts
